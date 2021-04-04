e629e9b275ec1ae677a06bf9f1e724b690543aa3
Raul Sanz <luar_znas@hotmail.com>
2021-04-04 19:49:05 +0100

    HTML: Styled the nav header menu and linked the index btns

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,17 @@ function getTheMovieDatabase() {
   var API_KEY = "7557a7686c1be5c7114f3c419653ff79";
   var type = "movie"; // or "tv"
   var keyword = "12377"; // looking for keywords for each subGenre
-  var year = "2020"; 
+  var year = "2020";
   var language = "ko"; // make a function to convert the language to ISO_639-1 code
-  // Zombies ID: 12377
-  // Gore ID: 10292
   // Psychological ID: 157314
   // Post Apocalyptic ID: 270348
-  // Slasher: 12339
-  // + Serial Killer ID: 10714
+  // Slasher: 12339   // + Serial Killer ID: 10714
+  // Supernatural ID: 6152
+  // Paranormal ID: 9853
+  // Zombies ID: 12377
+  // Monsters ID: 1299
+  // Gore ID: 10292
+  // Reigion ID: 11001
 
   fetch(
     "https://api.themoviedb.org/3/discover/" +
