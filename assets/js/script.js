bde601645a81848dc00011b04ec41f0833f0a5cb
Thom Williams <thomwilliams1990@gmail.com>
2021-04-05 20:34:58 +0100

    JS + CSS: Conflicts resolved

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 // Getting the movie criteria
 var typeEl = document.querySelector("#type");
 var subgenreEl = document.querySelector("#subgenre");
@@ -5,10 +7,15 @@ var yearsEl = document.querySelector("#years");
 var languageEl = document.querySelector("#language");
 var movieselectorEl = document.querySelector("#movieselector");
  
+function hideEls () {
 subgenreEl.style.display = "none";
 yearsEl.style.display = "none";
 languageEl.style.display = "none";
 movieselectorEl.style.display = "none";
+
+}
+
+hideEls
  
 var API_KEY = "7557a7686c1be5c7114f3c419653ff79";
 var urlForm = "https://api.themoviedb.org/3/discover/";
@@ -135,83 +142,106 @@ function printItemList(movieData) {
    movieSelectorContainerEl.appendChild(itemWrapperEl);
  }
 }
- 
+
+
+
 // OPEN MOVIE DATABASE
+ 
 function getOpenMovieDatabaseAPI() {
-var API_KEY = "930706b3";
-var title = "the ring";
-var year = "2002";
-var requestURL = `http://www.omdbapi.com/?t=${title}&y=${year}&apikey=${API_KEY}`;
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log("OMDB", data);
-    trickorTreat(data);
-    displaySelectedMovie(data)
-  });
+  var API_KEY = "930706b3";
+  var title = "the ring";
+  var year = "2002";
+  var requestURL = `http://www.omdbapi.com/?t=${title}&y=${year}&apikey=${API_KEY}`;
+  
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("OMDB", data);
+  
+      trickorTreat(data);
+      displaySelectedMovie(data)
+    });
 }
+  
 getOpenMovieDatabaseAPI();
+  
+  
 // TRICK OR TREAT FUNCTION
 function trickorTreat (data) {
-var IMDBscore = data.Ratings[0].value;
-var rottenTomatoesScore = data.Ratings[1].value;
-var metacriticScore = data.Ratings[2].value;
-var trickOrTreatInput = document.getElementById("trickortreat");
-if ((IMDBscore > parseInt("5.0/10", 5/10) && rottenTomatoesScore > parseInt("50%", 50/100) && metacriticScore > parseInt("50/100", 50/100))) {
-  trickOrTreatInput.textContent = " TREAT!";
-  trickOrTreatInput.setAttribute("id", "treat");
-} else if ((IMDBscore < parseInt("5.0/10", 5/10) && rottenTomatoesScore < parseInt("50%", 50/100) && metacriticScore < parseInt("50/100", 50/100))) {
-  trickOrTreatInput.textContent = " TRICK!";
-  trickOrTreatInput.setAttribute("id", "trick");
-} else {
-  trickOrTreatInput.textContent = " JURY'S OUT - APPROACH WITH CAUTION!";
-  trickOrTreatInput.setAttribute("id", "caution");
+  var IMDBscore = data.Ratings[0].value;
+  var rottenTomatoesScore = data.Ratings[1].value;
+  var metacriticScore = data.Ratings[2].value;
+  var trickOrTreatInput = document.getElementById("trickortreat");
+  if ((IMDBscore > parseInt("5.0/10", 5/10) && rottenTomatoesScore > parseInt("50%", 50/100) && metacriticScore > parseInt("50/100", 50/100))) {
+    trickOrTreatInput.textContent = " TREAT!";
+    trickOrTreatInput.setAttribute("id", "treat");
+  } else if ((IMDBscore < parseInt("5.0/10", 5/10) && rottenTomatoesScore < parseInt("50%", 50/100) && metacriticScore < parseInt("50/100", 50/100))) {
+    trickOrTreatInput.textContent = " TRICK!";
+    trickOrTreatInput.setAttribute("id", "trick");
+  } else {
+    trickOrTreatInput.textContent = " JURY'S OUT - APPROACH WITH CAUTION!";
+    trickOrTreatInput.setAttribute("id", "caution");
+  }
+ }
+  
+  
+ function displaySelectedMovie(data) {
+  console.log(data, "displayselectedmovie")
+  
+  var filmTitle = document.getElementById("film-title");
+  filmTitle.textContent = data.Title;
+  var posterImage = document.getElementById("poster");
+  var posterURL = data.Poster;
+  posterImage.setAttribute("src", posterURL);
+  var directorName = document.getElementById("director");
+  directorName.textContent = data.Director;
+  directorName.style.color = "orange";
+  var ageCertificate = document.getElementById("age");
+  ageCertificate.textContent = data.Rated;
+  ageCertificate.style.color = "orange";
+  var countryLanguage = document.getElementById("country");
+  countryLanguage.textContent = data.Country + " / " + data.Language;
+  countryLanguage.style.color = "orange";
+  var runTime = document.getElementById("runtime");
+  runTime.textContent = data.Runtime;
+  runTime.style.color = "orange";
+  var yearReleased = document.getElementById("year-released");
+  yearReleased.textContent = data.Released;
+  yearReleased.style.color = "orange";
+  var availableOn = document.getElementById("watchon");
+  availableOn.textContent;
+  var filmSynopsis = document.getElementById("synopsis");
+  filmSynopsis.textContent = data.Plot;
+  filmSynopsis.style.color = "black";
+  
 }
-}
-function displaySelectedMovie(data) {
-console.log(data, "displayselectedmovie")
-var filmTitle = document.getElementById("film-title");
-filmTitle.textContent = data.Title;
-var posterImage = document.getElementById("poster");
-var posterURL = data.Poster;
-posterImage.setAttribute("src", posterURL);
-var directorName = document.getElementById("director");
-directorName.textContent = data.Director;
-directorName.style.color = "orange";
-var ageCertificate = document.getElementById("age");
-ageCertificate.textContent = data.Rated;
-ageCertificate.style.color = "orange";
-var countryLanguage = document.getElementById("country");
-countryLanguage.textContent = data.Country + " / " + data.Language;
-countryLanguage.style.color = "orange";
-var runTime = document.getElementById("runtime");
-runTime.textContent = data.Runtime;
-runTime.style.color = "orange";
-var yearReleased = document.getElementById("year-released");
-yearReleased.textContent = data.Released;
-yearReleased.style.color = "orange";
-var availableOn = document.getElementById("watchon");
-availableOn.textContent;
-var filmSynopsis = document.getElementById("synopsis");
-filmSynopsis.textContent = data.Plot;
-filmSynopsis.style.color = "black";
-}
-function runModal () {
-var errorModal = document.getElementById("errorModal");
-var modalSpan = document.getElementById("close"[0]);
-// Button to Test Modal - remove this later...
-var modalBtn = document.getElementById("modalBtn");
-modalBtn.onclick = function () {
-errorModal.style.display = "block";
-}
-modalSpan.onclick = function () {
-errorModal.style.display = "none";
-}
-window.onclick = function(event) {
-if (event.target == modal) {
-modal.style.display = "none";
-}}
-}
-runModal()
+  
+  
+  
+  
+// function runModal () {
+//  var errorModal = document.getElementById("errorModal");
+//  var modalSpan = document.getElementById("close"[0]);
+  
+// //  // Button to Test Modal - remove this later...
+// //  var modalBtn = document.getElementById("modalBtn");
+// //  modalBtn.onclick = function () {
+// //   errorModal.style.display = "block";
+// //  }
+  
+//  modalSpan.onclick = function () {
+//   errorModal.style.display = "none";
+  
+//  }
+  
+//  window.onclick = function(event) {
+//  if (event.target == modal) {
+//   modal.style.display = "none";
+//  }}
+// }
+  
+// runModal()
+
+ 
